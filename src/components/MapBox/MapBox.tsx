29bb7413fc34e28b99b0c9d4e51aef75c439e3f7
Manjot Virdi <virdi_manjot@hotmail.com>
2023-05-11 14:29:29 -0700

    update mapbox component styling; apply default styling to app.scss

--- src/components/MapBox/MapBox.tsx
+++ src/components/MapBox/MapBox.tsx
@@ -4,21 +4,23 @@ import "mapbox-gl/dist/mapbox-gl.css";
 
 const MAPBOX_TOKEN = process.env.REACT_APP_MAPBOX_TOKEN;
 
-function MapBox() {
+function MapBox(): JSX.Element {
     return (
-        <Map
-            initialViewState={{
-                latitude: 37.8,
-                longitude: -122.4,
-                zoom: 14,
-            }}
-            style={{ width: 800, height: 600 }}
-            mapStyle="mapbox://styles/mapbox/light-v11"
-            mapboxAccessToken={MAPBOX_TOKEN}
-            projection="globe"
-        >
-            <Marker longitude={-122.4} latitude={37.8} color="red" />
-        </Map>
+        <section className="map-container">
+            <Map
+                initialViewState={{
+                    latitude: 37.8,
+                    longitude: -122.4,
+                    zoom: 2,
+                }}
+                style={{ width: 800, height: 600 }}
+                mapStyle="mapbox://styles/mapbox/light-v11"
+                mapboxAccessToken={MAPBOX_TOKEN}
+                projection="globe"
+            >
+                <Marker longitude={-122.4} latitude={37.8} color="red" />
+            </Map>
+        </section>
     );
 }
 
