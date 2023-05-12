cb574d35f569575c379034cc3825cb6c06191504
Manjot Virdi <virdi_manjot@hotmail.com>
2023-05-12 11:23:35 -0700

    update styling of switch; add use state for switch

--- src/components/ViewSwitch/ViewSwitch.tsx
+++ src/components/ViewSwitch/ViewSwitch.tsx
@@ -1,13 +1,13 @@
+import { useState } from "react";
 import "./ViewSwitch.scss";
 
 function ViewSwitch(): JSX.Element {
-    const handleSwitchChange = () => {
-        console.log("It worked");
-    };
+    const [map, setMap] = useState<boolean>(true);
+
     return (
         <label htmlFor="view" className="switch">
             Map or List View
-            <div className="switch__container">
+            <div className={`switch__container ${map ? "" : "switch__container--list"}`}>
                 <div className="switch__item"></div>
             </div>
             <input
@@ -15,7 +15,7 @@ function ViewSwitch(): JSX.Element {
                 name="view"
                 id="view"
                 className="switch__input"
-                onChange={handleSwitchChange}
+                onChange={() => setMap(!map)}
             />
         </label>
     );
