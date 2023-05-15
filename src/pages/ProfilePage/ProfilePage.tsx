baac74ab590a88077e351d3a027a6e06963e9300
Manjot Virdi <virdi_manjot@hotmail.com>
2023-05-15 13:00:41 -0700

    update profile header formatting

--- src/pages/ProfilePage/ProfilePage.tsx
+++ src/pages/ProfilePage/ProfilePage.tsx
@@ -6,14 +6,18 @@ function ProfilePage(): JSX.Element {
             <div className="profile__header">
                 <div className="profile__cover-img">
                     <div className="profile__img"></div>
-                    <h1>Welcome **USER**</h1>
+                    <h1 className="profile__title">Welcome **USER**</h1>
                 </div>
                 <ul className="profile__sub-nav">
-                    <li className="profile_nav-item">Map</li>
-                    <li className="profile_nav-item">Posts</li>
-                    <li className="profile_nav-item">Gallery</li>
-                    <li className="profile_nav-item">About</li>
+                    <li className="profile__nav-item">Map</li>
+                    <li className="profile__nav-item">Gallery</li>
+                    <li className="profile__nav-item">About</li>
                 </ul>
+            </div>
+            <div className="profile__content">
+                {/* Map */}
+                {/* Gallery */}
+                {/* About */}
             </div>
         </section>
     );
