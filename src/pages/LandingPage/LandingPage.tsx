15034b81bcea075a16029fd8d964cbff98f8375c
Manjot Virdi <virdi_manjot@hotmail.com>
2023-05-25 11:23:46 -0700

    define interfaces that will be returned from getCollections api call

--- src/pages/LandingPage/LandingPage.tsx
+++ src/pages/LandingPage/LandingPage.tsx
@@ -1,10 +1,29 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import ViewCollections from "../../components/ViewCollections/ViewCollections";
 import "./LandingPage.scss";
 import axios from "axios";
 import { apiUrl } from "../../App";
 
+//Type Definitions
+interface CollectionImages {
+    id: string;
+    title: string;
+    image: string;
+    imageUrl: string;
+    latitude: number;
+    longitude: number;
+}
+
+interface Collections {
+    id: string;
+    title: string;
+    description: string;
+    user_id: string;
+    collection_images: CollectionImages[];
+}
+
 function LandingPage(): JSX.Element {
+    const [collections, setCollections] = useState<Collections[]>([]);
     useEffect(() => {
         getCollections();
     }, []);
