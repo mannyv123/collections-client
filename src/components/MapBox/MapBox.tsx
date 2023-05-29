fc53c5c374aaa50195b220469fea1d040a3b05b1
Manjot Virdi <virdi_manjot@hotmail.com>
2023-05-29 14:58:31 -0700

    update mapbox component to ask and set view state to user location

--- src/components/MapBox/MapBox.tsx
+++ src/components/MapBox/MapBox.tsx
@@ -2,6 +2,7 @@ import "./MapBox.scss";
 import Map, { Marker } from "react-map-gl";
 import "mapbox-gl/dist/mapbox-gl.css";
 import { Collections } from "../../types/types";
+import { useEffect, useRef, useState } from "react";
 
 interface MapBoxProps {
     collections: Collections[];
@@ -10,16 +11,43 @@ interface MapBoxProps {
 const MAPBOX_TOKEN = process.env.REACT_APP_MAPBOX_TOKEN;
 
 function MapBox({ collections }: MapBoxProps): JSX.Element {
+    // const [currentPos, setCurrentPos] = useState({ latitude: 37.8, longitude: -122.4, zoom: 2 });
+    const [viewState, setViewState] = useState({ latitude: 37.8, longitude: -122.4, zoom: 2 });
+
     console.log(collections);
+
+    // useEffect(() => {
+    //     getLocation();
+    // }, []);
+
+    const getLocation = () => {
+        function success(position: GeolocationPosition) {
+            const latitude = position.coords.latitude;
+            const longitude = position.coords.longitude;
+            setViewState({ latitude, longitude, zoom: 2 });
+        }
+
+        function error() {
+            setViewState({ latitude: 37.8, longitude: -122.4, zoom: 2 });
+        }
+
+        if (!navigator.geolocation) {
+            alert("Geolocation is not supported by your browser");
+        } else {
+            navigator.geolocation.getCurrentPosition(success, error);
+        }
+    };
+
+    // console.log(currentPos);
 
     return (
         <section className="map-container">
             <Map
-                initialViewState={{
-                    latitude: 37.8,
-                    longitude: -122.4,
-                    zoom: 2,
-                }}
+                // initialViewState={currentPos}
+                // {...currentPos}
+                {...viewState}
+                onLoad={getLocation}
+                onMove={(evt) => setViewState(evt.viewState)}
                 style={{ width: "100vw", height: "93.5vh" }}
                 mapStyle="mapbox://styles/mapbox/light-v11"
                 mapboxAccessToken={MAPBOX_TOKEN}
