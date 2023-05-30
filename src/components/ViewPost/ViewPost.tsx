7f94f5478b630250456497c603b83acc28e0f538
Manjot Virdi <virdi_manjot@hotmail.com>
2023-05-29 17:44:13 -0700

    create view post component

---
+++ src/components/ViewPost/ViewPost.tsx
@@ -0,0 +1,11 @@
+import "./ViewPost.scss";
+
+function ViewPost(): JSX.Element {
+    return (
+        <section className="view-post">
+            <h3 className="view-post__title">Post Title</h3>
+        </section>
+    );
+}
+
+export default ViewPost;
