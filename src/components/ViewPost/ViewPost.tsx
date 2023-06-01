e67024f23c6aaa78377bbff9f573eac90f59d132
Manjot Virdi <virdi_manjot@hotmail.com>
2023-06-01 14:39:50 -0700

    add container for other images; update styling of displaying images

--- src/components/ViewPost/ViewPost.tsx
+++ src/components/ViewPost/ViewPost.tsx
@@ -10,16 +10,30 @@ interface ViewPostProps {
 function ViewPost({ selected, selectedImgIndex }: ViewPostProps): JSX.Element {
     // const [currentImgIndex, setCurrentImgIndex] = useState<number>(selectedImgIndex);
     console.log(selected);
-
+    const otherImgs = selected.collection_images.filter(
+        (image) => image.id !== selected.collection_images[selectedImgIndex].id
+    );
     return (
         <section className="view-post">
             <h3 className="view-post__title">{selected.title}</h3>
             <p>{selected.user_id}</p>
-            <img
-                src={selected.collection_images[selectedImgIndex].imageUrl}
-                alt="selected"
-                className="view-post__selected-img"
-            />
+            <div className="view-post__imgs-container">
+                <img
+                    src={selected.collection_images[selectedImgIndex].imageUrl}
+                    alt="selected"
+                    className="view-post__selected-img"
+                />
+                <div className="view-post__other-imgs-container">
+                    {otherImgs.map((image) => (
+                        <img
+                            key={image.id}
+                            src={image.imageUrl}
+                            alt="remaining"
+                            className="view-post__other-imgs"
+                        />
+                    ))}
+                </div>
+            </div>
         </section>
     );
 }
