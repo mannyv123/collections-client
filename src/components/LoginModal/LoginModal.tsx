612913f81d7b98a12705750654347c2b1429d3c3
Manjot Virdi <virdi_manjot@hotmail.com>
2023-06-07 21:58:51 -0700

    update login modal to save username to local storage

--- src/components/LoginModal/LoginModal.tsx
+++ src/components/LoginModal/LoginModal.tsx
@@ -1,11 +1,33 @@
-import { RefObject } from "react";
+import { FormEvent, RefObject, useState } from "react";
 import "./LoginModal.scss";
+import { useNavigate } from "react-router-dom";
 
 interface LoginModalProps {
     loginDialogRef: RefObject<HTMLDialogElement>;
 }
 
+const initialValues = {
+    username: "",
+    password: "",
+};
+
 function LoginModal({ loginDialogRef }: LoginModalProps): JSX.Element {
+    const navigate = useNavigate();
+    const [loginInputs, setLoginInputs] = useState(initialValues);
+
+    const handleInputChange = (event: FormEvent<HTMLInputElement>) => {
+        const { name, value } = event.currentTarget;
+        setLoginInputs({ ...loginInputs, [name]: value });
+    };
+
+    const handleLoginFormSubmit = (event: FormEvent) => {
+        event.preventDefault();
+
+        localStorage.setItem("username", loginInputs.username);
+        navigate(`/${loginInputs.username}/map`);
+        loginDialogRef.current?.close();
+    };
+
     return (
         <dialog ref={loginDialogRef} className="login">
             <div className="login__container">
@@ -18,13 +40,15 @@ function LoginModal({ loginDialogRef }: LoginModalProps): JSX.Element {
                     X
                 </p>
                 <h1 className="login__title">Login</h1>
-                <form action="submit" className="login__form">
+                <form action="submit" className="login__form" onSubmit={handleLoginFormSubmit}>
                     <input
                         type="text"
                         name="username"
                         id="username"
                         className="login__input"
                         placeholder="Username"
+                        onChange={handleInputChange}
+                        value={loginInputs.username}
                     />
                     <input
                         type="password"
@@ -32,6 +56,8 @@ function LoginModal({ loginDialogRef }: LoginModalProps): JSX.Element {
                         id="password"
                         className="login__input"
                         placeholder="Password"
+                        onChange={handleInputChange}
+                        value={loginInputs.password}
                     />
                     <button type="submit" className="login__submit">
                         Login
